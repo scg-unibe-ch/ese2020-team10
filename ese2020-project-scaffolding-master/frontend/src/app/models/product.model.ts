92db18f18b24824dc32e71fe69871f48b25bf639
Martin-Widmer <71330237+Martin-Widmer@users.noreply.github.com>
2020-10-20 20:43:21 +0200

    Add product Model

---
+++ ese2020-project-scaffolding-master/frontend/src/app/models/product.model.ts
@@ -0,0 +1,13 @@
+export class Product { 
+    constructor(
+        productId: number,
+        title: string,
+        price: number,
+        description: string,
+        location: string,
+        sellOrLend: boolean,
+        status: boolean,
+        shippable: boolean,
+        userId: number
+    ) {}
+}
