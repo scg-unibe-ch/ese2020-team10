44ec280cd0bbbb38e94de023c4be7d0810af646f
Martin-Widmer <71330237+Martin-Widmer@users.noreply.github.com>
2020-10-20 20:41:16 +0200

    Update product.service.ts

--- ese2020-project-scaffolding-master/frontend/src/app/product.service.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/product.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor( private httpClien: HttpClient) { }
+
+  products : Observable<Product[]>;
+
+  getProducts() {
+    this.products = this.httpClien.get<Product[]>(environment.endpointURL+'product/productList');
+  }
+  
 }
