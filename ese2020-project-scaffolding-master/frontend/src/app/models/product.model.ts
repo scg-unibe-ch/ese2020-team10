ece06d00dbb968b38228dfa8c4232b693c19be16
Martin-Widmer <71330237+Martin-Widmer@users.noreply.github.com>
2020-10-27 17:20:26 +0100

    Fix bug

--- ese2020-project-scaffolding-master/frontend/src/app/models/product.model.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/models/product.model.ts
@@ -1,6 +1,18 @@
 import { ɵAPP_ID_RANDOM_PROVIDER } from '@angular/core'
 
 export class Product { 
+    productId: number;
+    category: Category;
+    title: string;
+    price: number;
+    description: string;
+    location: string;
+    type: Type;
+    sellOrLend: boolean;
+    status: boolean;
+    shippable: boolean;
+    userId: number;
+    approved: boolean;
     constructor(
         productId: number,
         category: Category,
