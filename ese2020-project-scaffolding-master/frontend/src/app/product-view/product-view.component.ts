2052146d5325bf64c4d14110fa873cdff5925521
Korab Bejta <korab.bejta@students.unibe.ch>
2020-10-27 10:02:20 +0100

    added ngIfs to product-view.component.html

--- ese2020-project-scaffolding-master/frontend/src/app/product-view/product-view.component.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/product-view/product-view.component.ts
@@ -1,8 +1,23 @@
 import {Component} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
   styleUrls: ['./product-view.component.css'],
 })
-export class ProductViewComponent {}
+export class ProductViewComponent {
+
+  productId = null;
+  title = 'asnee';
+  price = null;
+  description = '';
+  location = '';
+  sellOrLend = false;
+  status = false;
+  deliverable = false;
+
+  constructor(private httpClient: HttpClient) { }
+
+
+}
