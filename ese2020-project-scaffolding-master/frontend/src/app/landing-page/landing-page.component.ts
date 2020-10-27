074716481c68da410b09ba56db06d2382eac76b9
Melina Meyer <melina.meyer@students.unibe.ch>
2020-10-27 14:41:47 +0100

    add product call in lend and hire

--- ese2020-project-scaffolding-master/frontend/src/app/landing-page/landing-page.component.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/landing-page/landing-page.component.ts
@@ -14,12 +14,17 @@ export class LandingPageComponent implements OnInit {
   user: string;
   isAdmin: string;
   public sellProducts: Observable<Product[]>;
+  public lendProducts: Observable<Product[]>;
+  public hireProducts: Observable<Product[]>;
+
 
   constructor(public auth: AuthService, public productService : ProductService) { }
 
   ngOnInit(): void {
     this.user = this.auth.getUserName();
-    this.sellProducts = this.productService.getProductsByType('Sell'); // Getts all the current 'sell' products from the backend via productService
+    this.sellProducts = this.productService.getProductsByType('Sell'); // Gets all the current 'sell' products from the backend via productService
+    this.lendProducts = this.productService.getProductsByType('Lend');
+    this.hireProducts = this.productService.getProductsByType('Hire');
   }
 
 }
