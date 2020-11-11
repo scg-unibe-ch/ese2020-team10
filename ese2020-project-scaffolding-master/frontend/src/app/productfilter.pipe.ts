bb175fbd827f3b9bdf1bec7064f39485b3eb441f
Martin-Widmer <71330237+Martin-Widmer@users.noreply.github.com>
2020-11-11 01:55:18 +0100

    Fixed Filtering
    Filtering works, removed dollar signs from prices in DB.

--- ese2020-project-scaffolding-master/frontend/src/app/productfilter.pipe.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/productfilter.pipe.ts
@@ -7,9 +7,7 @@ import { ProductService } from './product.service';
 })
 export class ProductfilterPipe implements PipeTransform {
 
-  transform(Products: Product[], searchText: String, location: String, maxPrice:number): Product[] {
-    console.log(Product[0].price)
-    console.log(Products[0].price <= maxPrice)
+  transform(Products: Product[], searchText: String, location: String, maxPrice:number, minPrice:number , availability:boolean): Product[] {
     if(!Products)
       return Products;
     
@@ -18,18 +16,18 @@ export class ProductfilterPipe implements PipeTransform {
     
     if(!location)
       location = ""
-    //console.log(maxPrice)
-    //console.log(100 <= maxPrice)
-    /**if(!minPrice)
+
+    if(!minPrice)
       minPrice = 0
     
     if(!maxPrice)
       maxPrice = Number.MAX_SAFE_INTEGER
-*/
+
     return Products.filter(product =>
-      product.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) && product.price <= maxPrice && product.location.toLocaleLowerCase().includes(location.toLocaleLowerCase())
-      //&& product.price >= minPrice && product.price <= maxPrice
-      );
+      product.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) 
+      && product.price <= maxPrice && product.price >= minPrice
+      && product.location.toLocaleLowerCase().includes(location.toLocaleLowerCase())
+      &&(availability || product.shippable));
   }
   /**
    * transform(Products: Product[], searchText: String, minPrice: Number, maxPrice: Number, location: String, availability: boolean): Product[] {
