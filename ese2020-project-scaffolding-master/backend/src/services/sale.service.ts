f976d5281716738803d57a20147fdfd5fdd8de7d
Martin-Widmer <71330237+Martin-Widmer@users.noreply.github.com>
2020-11-18 00:28:22 +0100

    make buy in backend more resilient
    buy functionality works now with fewer inputs from the frontend,
    added delivery address,
    fixed bug with shippable/status confusion

--- ese2020-project-scaffolding-master/backend/src/services/sale.service.ts
+++ ese2020-project-scaffolding-master/backend/src/services/sale.service.ts
@@ -2,40 +2,65 @@ import { UserAttributes, User } from '../models/user.model';
 import { LoginResponse, LoginRequest } from '../models/login.model';
 import { Product} from '../models/product.model';
 import { SaleAttributes, Sale } from '../models/sale.model';
+import { PurchaseRequest, PurchaseResponse } from '../models/purchase.model';
+import { Transaction } from 'sequelize/types';
+import { Server} from '../server';
 
 
+
+// const sequelize = require('sequelize');
 
 export class SaleService {
+    async buy(purchaseRequest: PurchaseRequest, buyerId: number): Promise<Sale | PurchaseResponse> {
+        try {
+            // find the product to sell
+            const product = await Product.findByPk(purchaseRequest.productId);
+            // make sure the product is there
+            if (product == null) {
+                throw new Error('Product not found');
+            }
+            // make sure product is shippable
+            if (!product.shippable) {
+                throw new Error('Product is not shippable');
+            }
+            // find buyer
+            const buyer = await User.findByPk(buyerId);
+            // make sure the buyer has sufficient money
+            if (buyer.wallet < product.price) {
+                throw new Error('Insufficient funds');
+            }
+            // find seller
+            const seller = await User.findByPk(product.userId);
 
-    public buy(sale: SaleAttributes, buyerId: number): Promise<SaleAttributes> {
-        // find the product to sell
-        Product.findByPk(sale.productId).then(foundProduct => {
-            if (foundProduct != null) {
-                // set the current sale price
-                sale.pointOfSalePrice = foundProduct.price;
+            // cant use transaction yet because it requires sequelize
+            try {
                 // make product unavailable
-                foundProduct.update({shippable: false}).then();
-                // find buyer and decrease its waller by sellingprice
-                User.findByPk(buyerId).then(foundBuyer => {
-                    if (foundBuyer != null) {
-                        foundBuyer.decrement(['wallet'], {by: sale.pointOfSalePrice});
-                    } else {
-                        return Promise.reject({ message: 'buyer not found'});
-                    }
+                await product.update({shippable: false});
+
+                // decrease the buyers wallet by sellingprice
+                await buyer.update({wallet: buyer.wallet - product.price});
+
+                // increase the sellers wallet by sellingprice
+                await seller.update({wallet: seller.wallet + product.price});
+
+                // make a entry in the sales table
+                const sale = await Sale.create({
+                    'productId': product.productId,
+                    'buyerId': buyerId,
+                    'sellerId': seller.userId,
+                    'pointOfSalePrice': product.price,
+                    'deliveryAddress': purchaseRequest.deliveryAddress
                 });
-                // find yeller and increase its wallet by sellingprice
-                User.findByPk(foundProduct.userId).then(foundSeller => {
-                    if (foundSeller != null) {
-                        foundSeller.increment(['wallet'], {by: sale.pointOfSalePrice});
-                    } else {
-                        return Promise.reject({ message: 'seller not found'});
-                    }
-                });
-            } else {
-                return Promise.reject({ message: 'product not found'});
+
+                return Promise.resolve(sale);
+            } catch (err) {
+                throw new Error('');
             }
 
-        });
-        return Sale.create(sale).then(inserted => Promise.resolve(inserted)).catch(err => Promise.reject(err));
+
+        } catch (err) {
+            console.log(err);
+            return Promise.reject({message: err.toString()});
+        }
     }
 }
