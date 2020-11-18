5603bf2e403c8d061f184fc44d406ec1732f350a
Martin-Widmer <71330237+Martin-Widmer@users.noreply.github.com>
2020-11-18 12:18:51 +0100

    implement amount of hours in backend
    If there is an amount of hours, the price is interpreted as price per hour and multiplied by amount of hours.

--- ese2020-project-scaffolding-master/backend/src/services/sale.service.ts
+++ ese2020-project-scaffolding-master/backend/src/services/sale.service.ts
@@ -23,10 +23,16 @@ export class SaleService {
             if (!product.status) {
                 throw new Error('Product is not shippable');
             }
+            // find out the Price of this Purchase
+            let price: number = product.price;
+            if (purchaseRequest.amountOfHours != null) {
+                price = price * purchaseRequest.amountOfHours;
+            }
+
             // find buyer
             const buyer = await User.findByPk(buyerId);
             // make sure the buyer has sufficient money
-            if (buyer.wallet < product.price) {
+            if (buyer.wallet < price) {
                 throw new Error('Insufficient funds');
             }
             // find seller
@@ -38,20 +44,21 @@ export class SaleService {
                 await product.update({status: false});
 
                 // decrease the buyers wallet by sellingprice
-                await buyer.update({wallet: buyer.wallet - product.price});
+                await buyer.update({wallet: buyer.wallet - price});
 
                 // increase the sellers wallet by sellingprice
-                await seller.update({wallet: seller.wallet + product.price});
+                await seller.update({wallet: seller.wallet + price});
 
                 // make a entry in the sales table
                 const sale = await Sale.create({
                     'productId': product.productId,
                     'buyerId': buyerId,
                     'sellerId': seller.userId,
-                    'pointOfSalePrice': product.price,
+                    'pointOfSalePrice': price,
                     'deliveryAddress': purchaseRequest.deliveryAddress,
                     'title': product.title,
-                    'type': product.type
+                    'type': product.type,
+                    'amountOfHours': purchaseRequest.amountOfHours
                 });
 
                 return Promise.resolve(sale);
