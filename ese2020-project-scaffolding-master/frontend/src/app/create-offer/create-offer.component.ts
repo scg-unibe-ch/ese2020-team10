f65873579ed742465e2a4d71e8f950fa7a4175cc
Melina Meyer <melina.meyer@students.unibe.ch>
2020-11-18 11:08:25 +0100

    update create offer UI and fix bugs

--- ese2020-project-scaffolding-master/frontend/src/app/create-offer/create-offer.component.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/create-offer/create-offer.component.ts
@@ -21,7 +21,7 @@ export class CreateOfferComponent implements OnInit {
   @Input() categories = categoryTypes;
   userId: string;
   product: Product;
-  
+
   constructor(private httpClient: HttpClient, private router: Router, private currencyPipe: CurrencyPipe, public auth: AuthService) { }
 
   ngOnInit(): void {
@@ -30,7 +30,7 @@ export class CreateOfferComponent implements OnInit {
     this.type.setValue('Sell');
     this.selectedCategory.setValue(this.categories[0]);
     this.shippable.setValue('false');
-  }  
+  }
 
   createOfferForm = new FormGroup({
     selectedCategory: new FormControl(),
@@ -40,14 +40,12 @@ export class CreateOfferComponent implements OnInit {
     description: new FormControl('', [
       Validators.required]),
     price: new FormControl('', [
-      Validators.required]),
-    pictureLink: new FormControl('', [
-      Validators.required]),
+      Validators.required, Validators.pattern("(\\d+(\\.\\d{1,2})?)")]),
     location: new FormControl(),
     type: new FormControl(),
     shippable: new FormControl(),
   });
- 
+
   get selectedCategory() {
     return this.createOfferForm.get('selectedCategory');
   }
@@ -63,9 +61,6 @@ export class CreateOfferComponent implements OnInit {
   get price() {
     return this.createOfferForm.get('price');
   }
-  get pictureLink() {
-    return this.createOfferForm.get('pictureLink');
-  }
   get location() {
     return this.createOfferForm.get('location');
   }
@@ -80,15 +75,15 @@ export class CreateOfferComponent implements OnInit {
     this.userId =  this.auth.getUserId();
     if(this.createOfferForm.valid){
       this.httpClient.post(environment.endpointURL + 'product/add', {
-        "category": this.selectedCategory.value,
-        "title": this.title.value,
-        "description": this.description.value,
-        "price": this.price.value,
-        "location": this.location.value,
-        "type": this.type.value,
-        "shippable": this.shippable.value,
-        "pictureLink": this.pictureLink.value,
-        "userId": this.userId
+        category: this.selectedCategory.value,
+        title: this.title.value,
+        description: this.description.value,
+        price: this.price.value,
+        location: this.location.value,
+        type: this.type.value,
+        shippable: this.shippable.value,
+        status: true,
+        userId: this.userId
       })
       .subscribe((res: any) => {
         this.router.navigate(['']);
@@ -111,12 +106,12 @@ export class CreateOfferComponent implements OnInit {
     return this.productOrService.value === 'Product';
   }
 
-priceFormat(): string {
-  if(this.type.value === 'Sell') {
-    return 'Price';
-  } else {
-    return 'Price per Hour'
+  priceFormat(): string {
+    if(this.type.value === 'Sell') {
+      return 'Price';
+    } else {
+      return 'Price per Hour';
+    }
   }
-}
 
 }
