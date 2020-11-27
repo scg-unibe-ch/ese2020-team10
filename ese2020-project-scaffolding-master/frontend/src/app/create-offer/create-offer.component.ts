66e399f54066bbdc0795abf86a14a540093b5751
Martin-Widmer <71330237+Martin-Widmer@users.noreply.github.com>
2020-11-27 14:15:34 +0100

    implemented Picture in frontend
    Pictures can now be added to Products, but are not displayed on the Product component yet

--- ese2020-project-scaffolding-master/frontend/src/app/create-offer/create-offer.component.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/create-offer/create-offer.component.ts
@@ -44,7 +44,20 @@ export class CreateOfferComponent implements OnInit {
     location: new FormControl(),
     type: new FormControl(),
     shippable: new FormControl(),
+    file: new FormControl(''),
+    fileSource: new FormControl('')
   });
+
+
+  onFileChange(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.createOfferForm.patchValue({
+        fileSource: file
+      });
+    }
+  }
+
 
   get selectedCategory() {
     return this.createOfferForm.get('selectedCategory');
@@ -71,24 +84,13 @@ export class CreateOfferComponent implements OnInit {
     return this.createOfferForm.get('shippable');
   }
 
-  onSubmit(): void {
-    this.userId =  this.auth.getUserId();
-    if(this.createOfferForm.valid){
-      this.httpClient.post(environment.endpointURL + 'product/add', {
-        category: this.selectedCategory.value,
-        title: this.title.value,
-        description: this.description.value,
-        price: this.price.value,
-        location: this.location.value,
-        type: this.type.value,
-        shippable: this.shippable.value,
-        status: true,
-        userId: this.userId
-      })
-      .subscribe((res: any) => {
+  onSubmit(){
+    const formData = new FormData();
+    formData.append('productImage', this.createOfferForm.get('fileSource').value);
+
+    this.httpClient.post(environment.endpointURL + 'product/newProduct', formData).subscribe((res: any) => {
         this.router.navigate(['']);
     });
-   }
   }
 
   isService(): boolean {
