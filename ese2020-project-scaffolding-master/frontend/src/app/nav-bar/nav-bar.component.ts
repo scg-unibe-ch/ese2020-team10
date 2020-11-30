c0bd7648674b8e42ec5a9a2b1380cd33ab206db7
Marc Guenter <marc.guenter99@gmail.com>
2020-11-30 21:14:56 +0100

    fixed bugs
    fixed input fields in registration form. fixed bug where user remained on create offer page after clicking Logout.

--- ese2020-project-scaffolding-master/frontend/src/app/nav-bar/nav-bar.component.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import {AuthService} from '../auth.service';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -12,7 +13,7 @@ export class NavBarComponent implements OnInit {
   loggedIn= new Observable<boolean>();
   isAdmin=  new Observable<boolean>();
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.auth.checkUserStatus();
@@ -23,6 +24,7 @@ export class NavBarComponent implements OnInit {
 
   onLogout(){
     this.auth.logout();
+    this.router.navigate(['']);
   }
   getUserName(){
     return this.auth.getUserName();
