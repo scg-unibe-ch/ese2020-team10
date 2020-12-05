4b59346668401786fb97af173e19d320a68968dd
Marc Guenter <marc.guenter99@gmail.com>
2020-12-05 20:56:10 +0100

    improved create review, deleted todolist

--- ese2020-project-scaffolding-master/frontend/src/app/app.module.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/app.module.ts
@@ -16,8 +16,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatRadioModule } from '@angular/material/radio';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthInterceptor } from './auth/auth.interceptor';
@@ -38,21 +36,20 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ProductfilterPipe } from './productfilter.pipe';
 import { PurchaseDialogComponent } from './product-item/purchase-dialog/purchase-dialog.component';
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatStepperModule} from "@angular/material/stepper";
-import {STEPPER_GLOBAL_OPTIONS} from "@angular/cdk/stepper";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatStepperModule } from "@angular/material/stepper";
+import { STEPPER_GLOBAL_OPTIONS } from "@angular/cdk/stepper";
 import { CategoryCatalogComponent } from './landing-page/category-catalog/category-catalog.component';
 import { ToastrModule } from 'ngx-toastr';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { ProfileUpdateComponent } from './profile/profile-update/profile-update.component';
 import { ProductDescriptionComponent } from './product-item/product-description/product-description.component';
 import { ProductDetailsComponent } from './product-item/product-description/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent,
-    TodoItemComponent,
     UserLoginComponent,
     UserRegistrationComponent,
     NavBarComponent,
@@ -96,7 +93,8 @@ import { ProductDetailsComponent } from './product-item/product-description/prod
     MatExpansionModule,
     MatStepperModule,
     CommonModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    NgbModule
   ],
   providers: [
     {
--- ese2020-project-scaffolding-master/frontend/src/app/product-item/product-description/product-description.component.ts
+++ ese2020-project-scaffolding-master/frontend/src/app/product-item/product-description/product-description.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
-import {ActivatedRoute} from "@angular/router";
-import {ProductService} from "../../product.service";
-import {Observable} from "rxjs";
-import {Product} from "../../models/product.model";
-import {environment} from "../../../environments/environment";
+import { ActivatedRoute } from "@angular/router";
+import { ProductService } from "../../product.service";
+import { Observable } from "rxjs";
+import { Product} from "../../models/product.model";
+import { environment } from "../../../environments/environment";
 import { take } from 'rxjs/operators';
 import { CdkTextareaAutosize } from '@angular/cdk/text-field';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../auth.service';
+import { ToastrService} from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-product-description',
@@ -23,8 +25,15 @@ export class ProductDescriptionComponent implements OnInit {
   endpointURL = environment.endpointURL;
   formGroup: FormGroup;
   loggedIn= new Observable<boolean>();
+  currentRate = 0;
 
-  constructor(private auth: AuthService, private httpClient: HttpClient, private route: ActivatedRoute, private productService: ProductService, private _ngZone: NgZone, private formBuilder: FormBuilder) { }
+  constructor(private auth: AuthService,
+    private httpClient: HttpClient,
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private _ngZone: NgZone,
+    private formBuilder: FormBuilder,
+    public toastr: ToastrService) {}
 
   ngOnInit(): void {
     this.productService.load();
@@ -34,7 +43,7 @@ export class ProductDescriptionComponent implements OnInit {
     this.product = this.productService.getProductsByProductId(this.productId.toString());
     this.loggedIn = this.auth.loggedIn;
     this.formGroup = this.formBuilder.group({
-      reviewText: ['', Validators.required]
+      reviewText: ['', Validators.required],
     });
   }
 
@@ -50,11 +59,14 @@ export class ProductDescriptionComponent implements OnInit {
     if(this.formGroup.valid) {
       this.httpClient.post(environment.endpointURL + 'review/', {
         productId: this.productId,
-        rating: 4,
+        rating: this.currentRate,
         reviewText: this.reviewText.value,
-        userName: 'hello'
-
-      }).subscribe((res: any) => {});
+      }).subscribe((res: any) => {
+        this.toastr.success('Review sent')
+      },
+      (error: any) => {
+        this.toastr.error('Review could not be sent')
+      });
     }
   }
 
